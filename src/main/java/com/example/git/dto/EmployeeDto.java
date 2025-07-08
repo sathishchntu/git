eaa3580c0e66ccb868787f688c088ad63ef89544
Mamidala <smamidala@galaxe.com>
2025-07-08 12:44:28 +0530

    second commit

--- src/main/java/com/example/git/dto/EmployeeDto.java
+++ src/main/java/com/example/git/dto/EmployeeDto.java
@@ -1,4 +1,44 @@
 package com.example.git.dto;
 
 public class EmployeeDto {
+
+    private int id;
+
+    private String name;
+
+    private String department;
+
+    private  int salary;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
 }
